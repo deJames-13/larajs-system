a7cda0e79adac4cde7eb7d3634fae082ab82072d
deJames-13 <de.james013@gmail.com>
2024-06-10 19:42:20 +0800

    dfa

--- database/migrations/2024_06_09_141318_create_categories_table.php
+++ database/migrations/2024_06_09_141318_create_categories_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
 
-            // TODO: Fill up columns
+
             $table->string('name');
             $table->text('description')->nullable();
             $table->string('image')->nullable();
