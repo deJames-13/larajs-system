018050814119a4f780a1337cc543d0abca1b7e13
deJames-13 <de.james013@gmail.com>
2024-06-25 21:25:30 +0800

    mbmb

---
+++ app/Http/Controllers/SearchController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Brand;
+use App\Models\Promos;
+use App\Models\Product;
+use App\Models\Category;
+use Illuminate\Http\Request;
+
+class SearchController extends Controller
+{
+    public function autocomplete()
+    {
+        $search = request()->get('search');
+        $searchIn = (object)[
+            'products' => Product::class,
+            'categories' => Category::class,
+            'brands' => Brand::class,
+            'promos' => Promos::class,
+        ];
+
+        $results = collect($searchIn)->map(function ($model) use ($search) {
+            return $model::filter(['search' => $search])->limit(10)->get();
+        });
+
+        return response()->json($results);
+    }
+}
