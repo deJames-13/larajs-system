109b09390371359f38167eacf79a14baa461cf6b
noellezzz <angelomiguel.dacumos@tup.edu.ph>
2024-07-02 14:16:11 +0800

    main page push

--- public/js/Dashboard/index.js
+++ public/js/Dashboard/index.js
@@ -1,5 +1,6 @@
 import Tables from "../Tables/tables.js";
 import DashboardSideBar from "./components/sidebar.js";
+import { mainPageStyles, injectStyles } from "./components/styles.js"; // Adjust the path as needed
 
 // TODO: adsjfklajdsfkljadsfkljal;dsfj ladsfjiamgoinginsane
 
@@ -10,6 +11,7 @@ class Dashboard {
     }
 
     init() {
+        injectStyles(mainPageStyles); // Inject the styles into the document
         DashboardSideBar.init({
             target: '#dashboard-sidebar',
             callback: this.getTable.bind(this)
@@ -18,15 +20,36 @@ class Dashboard {
 
     getTable(url) {
         $('#dashboard-content').html('');
-        url === 'products' && this.tables.getProducts();
-        url === 'brands' && this.tables.getBrands();
-        url === 'promos' && this.tables.getPromos();
-        url === 'categories' && this.tables.getCategories();
-        url === 'orders' && this.tables.getOrders();
+
+        if (url === 'main') {
+            this.loadMainPage();
+        } else {
+            url === 'products' && this.tables.getProducts();
+            url === 'brands' && this.tables.getBrands();
+            url === 'promos' && this.tables.getPromos();
+            url === 'categories' && this.tables.getCategories();
+            url === 'orders' && this.tables.getOrders();
+        }
+    }
+
+    loadMainPage() {
+        // Directly manipulate the DOM to load main page content
+        const mainPageContent = `
+            <div class="shapecontainer">
+                <div id="main-page">
+                    <div id="welcome">
+                        <h1>Welcome to</h1>
+                            <img src="/storage/images/GlitzVogue.png" alt="GLITZVOGUE Logo" class="glitzvogue-logo">
+                        <p>We cater to individuals regardless of gender to suffice your needs and wants in terms of cosmetic products</p>
+                    </div>
+                </div>
+                <div class="circle-container">
+                    ${Array(10).fill('<div class="circle"></div>').join('')}
+                </div>
+            </div>
+        `;
+        $('#dashboard-content').html(mainPageContent);
     }
 }
-
-
-
 
 new Dashboard();
