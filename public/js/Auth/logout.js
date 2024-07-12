8e63d074dbc24fb66a32c2a1e19df10127f266fd
deJames-13 <de.james013@gmail.com>
2024-07-12 21:58:09 +0800

    adsf

---
+++ public/js/Auth/logout.js
@@ -0,0 +1,24 @@
+import ajaxRequest from "../assets/ajaxRequest.js";
+
+const logout = () => {
+    return new Promise((resolve, reject) => {
+        const data = new FormData();
+        let csrfToken = document
+            .querySelector('meta[name="csrf-token"]')
+            .getAttribute("content");
+        data.append("_token", csrfToken);
+
+        ajaxRequest.post({
+            url: "/logout",
+            data: data,
+            onSuccess: (response) => {
+                resolve(response);
+            },
+            onError: (xhr) => {
+                reject(xhr);
+            },
+        });
+    });
+};
+
+export default logout;
