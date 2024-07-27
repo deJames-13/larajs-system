8b7a557434e77d8b9132a8700a8d01cbf64b9e34
deJames-13 <de.james013@gmail.com>
2024-07-27 18:21:00 +0800

    tofo fix broken scroll

--- public/js/components/SearchItem.js
+++ public/js/components/SearchItem.js
@@ -26,7 +26,6 @@ export default class SearchItem {
         class="search-item flex flex-col  p-4 cursor-pointer rounded-lg border-primary hover:bg-secondary hover:scale-95 hover:border-b-2 hover:text-primary hover:bg-opacity-20 hover:shadow-xl transition ease-in animated__animate animated__fadeInLeft"
       >
         <!--  <h3 class="font-bold text-lg">${this.index}</h3> -->
-        <h3 class="font-bold text-lg">${this.index}</h3>
         <h3 id="result-${this.id}-${this.index}" class="font-bold text-lg">${this.title}</h3>
         <h5 class="text-sm text-gray-600">${this.description}</h5>
         <h4 class="font-bold text-md">${this.bottomLabel}</h4>
